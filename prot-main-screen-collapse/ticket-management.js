bd106c299954e4a78aa87e8986d09904ab1d1d74
Arthur José Ferro Motter <t_arthur.motter@prefeitura.local>
2025-01-10 17:09:56 -0300

    alt version polish

--- prot-main-screen-collapse/ticket-management.js
+++ prot-main-screen-collapse/ticket-management.js
@@ -1,1 +1,10 @@
-console.log("teste");
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM carregado");
+    document.getElementById("menu-toggle").addEventListener("click", function() {
+        const menu = document.getElementById("collapsible-menu");
+        menu.classList.toggle("open");
+        /*debug*/ //console.log(menu.classList);  // Verifica as classes aplicadas
+    });
+    
+});
+
