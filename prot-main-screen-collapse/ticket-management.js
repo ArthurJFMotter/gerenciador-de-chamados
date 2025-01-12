89aef403ccfc49c011065278e496a175cca91126
ArthurJFMotter <arthur.jf.motter@gmail.com>
2025-01-12 09:14:07 -0300

    btn animation

--- prot-main-screen-collapse/ticket-management.js
+++ prot-main-screen-collapse/ticket-management.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
         const menu = document.getElementById("collapsible-menu");
         menu.classList.toggle("open");
         /*debug*/ //console.log(menu.classList);  // Verifica as classes aplicadas
+        const icon = document.getElementById("menu-toggle");
+        icon.classList.toggle("active");
     });
     
 });
