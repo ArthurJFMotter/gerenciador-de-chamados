1a7c59e4cd98763397fa7c1be0c2f67ef478ac83
Arthur José Ferro Motter <t_arthur.motter@prefeitura.local>
2025-01-13 17:52:05 -0300

    test cards

--- ticket-management-app/src/app/components/ticket-card/ticket-card.component.ts
+++ ticket-management-app/src/app/components/ticket-card/ticket-card.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+interface Ticket {
+  id: string;
+  status: string;
+  requester: string;
+  request: string;
+  location: string;
+  region: string;
+  date: string;
+  time: string;
+  responsible: string;
+}
 
 @Component({
   selector: 'app-ticket-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ticket-card.component.html',
   styleUrl: './ticket-card.component.css'
 })
-export class TicketCardComponent {
+export class TicketCardComponent implements OnInit {
+  tickets: Ticket[] = [
+        {
+            id: '00001',
+            status: '<i class="fa fa-star" style="color: red;"></i>',
+            requester: 'Isabela de Melo',
+            request: 'Instalação de Programas',
+            location: 'Secretaria de setor X',
+            region: 'INTERNA',
+            date: '01/01/2025',
+            time: '1h 33m',
+            responsible: 'Ciclano daqui',
+        },
+        {
+            id: '00002',
+            status: '<i class="fa fa-circle" style="color: yellow;"></i>',
+            requester: 'Miguel dos Santos',
+            request: 'Recuperação de Usuário',
+            location: 'Central de tal lugar',
+            region: 'CENTRO',
+            date: '04/01/2025',
+            time: '2d 5h',
+            responsible: 'Fulana de cá',
+        },
+        {
+            id: '00003',
+            status: '<i class="fa fa-circle" style="color: green;"></i>',
+            requester: 'João da Silva',
+            request: 'Instalação de impressora',
+            location: 'Hospital daquele canto ali',
+            region: 'NORTE',
+            date: '28/12/2024',
+            time: '4m',
+            responsible: 'Fulano de lá',
+        },
+    ];
+
+    getSafeHTML(html: string): any {
+        return html;
+    }
+
+  ngOnInit(): void {
+  }
 
 }
--- ticket-management-app/src/app/pages/ticket-manager/ticket-manager.component.ts
+++ ticket-management-app/src/app/pages/ticket-manager/ticket-manager.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { TicketTableComponent } from '../../components/ticket-table/ticket-table.component';
 import { CommonModule } from '@angular/common';
+import { TicketCardComponent } from '../../components/ticket-card/ticket-card.component';
 
 @Component({
   selector: 'app-ticket-manager',
-  imports: [CommonModule, TicketTableComponent],
+  imports: [CommonModule, TicketTableComponent, TicketCardComponent],
   templateUrl: './ticket-manager.component.html',
   styleUrl: './ticket-manager.component.css'
 })
