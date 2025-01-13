c97194d77ccd638127e845c38ddb787a90fc2bc9
Arthur José Ferro Motter <t_arthur.motter@prefeitura.local>
2025-01-13 14:50:36 -0300

    minimal fix

--- ticket-management-app/src/app/components/header/header.component.ts
+++ ticket-management-app/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  isMenuOpen: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
 }
