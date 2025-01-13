1ebe540839d0c447a22a823c0bf8ca9ba61e306a
Arthur José Ferro Motter <t_arthur.motter@prefeitura.local>
2025-01-13 14:06:05 -0300

    sec filter fix

--- prot-main-screen-dual/ticket-management.js
+++ prot-main-screen-dual/ticket-management.js
@@ -8,31 +8,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const mainContainer = document.getElementById("main-container");
 
 
-    // Function to toggle the collapsable menu
+    // nav collapsable menu
     menuToggle.addEventListener("click", function () {
         collapsibleMenu.classList.toggle("open");
         menuToggle.classList.toggle("active");
     });
 
 
-    // Function to toggle between table and card view
+    // table to card view
     toggleViewButton.addEventListener("click", function () {
-        // Toggle active class on the view toggle button
         toggleViewButton.classList.toggle("active");
 
-        // Toggle visibility for table and card grid
         ticketTable.style.display = (ticketTable.style.display === "none") ? "table" : "none";
         cardGrid.style.display = (cardGrid.style.display === "none") ? "grid" : "none";
 
-        // Change view mode according to current view
         toggleViewButton.innerHTML = (ticketTable.style.display === "none")
             ? '<i class="fa fa-table"></i> <span> Tabela </span>'
             : '<i class="fa fa-rectangle-list"></i> <span> Cartões </span>';
     });
 
-
-
-    // Sort table function
+    // Sort table
     const headers = ticketTable.querySelectorAll('th .table-header');
     let sortColumn = null;
     let sortOrder = 'asc';
@@ -71,8 +66,6 @@ document.addEventListener('DOMContentLoaded', function () {
         rows.forEach(row => tbody.appendChild(row));
     }
 
-    //Initial setup: Hide table, show cards on page load
     ticketTable.style.display = "none";
     cardGrid.style.display = "grid";
-
 });
