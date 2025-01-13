30d75e239c526ceec167f6e5ca1539142154409f
Arthur José Ferro Motter <t_arthur.motter@prefeitura.local>
2025-01-13 14:45:37 -0300

    fix

---
+++ ticket-management-app/src/app/components/ticket-table/ticket-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-table',
+  imports: [],
+  templateUrl: './ticket-table.component.html',
+  styleUrl: './ticket-table.component.css'
+})
+export class TicketTableComponent {
+
+}
