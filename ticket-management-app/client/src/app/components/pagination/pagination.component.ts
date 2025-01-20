5f57047c5ad9bcc0130b85dc3c624cfedfe5d305
Arthur José Ferro Motter <t_arthur.motter@prefeitura.local>
2025-01-20 16:28:40 -0300

    minor inprovement 2

--- ticket-management-app/client/src/app/components/pagination/pagination.component.ts
+++ ticket-management-app/client/src/app/components/pagination/pagination.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Ticket } from '../../services/ticket.service';
 
 @Component({
   selector: 'app-pagination',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.css'
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnChanges {
+  @Input() tickets: Ticket[] = [];
+  startItem: number = 0;
+  endItem: number = 0;
+  totalItems: number = 0;
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+
+ ngOnChanges(changes: SimpleChanges): void {
+   if (changes['tickets']) {
+     this.updatePagination();
+   }
+ }
+
+  updatePagination() {
+    this.totalItems = this.tickets.length;
+    this.startItem = (this.currentPage - 1) * this.itemsPerPage + 1;
+     this.endItem = Math.min(this.currentPage * this.itemsPerPage, this.totalItems);
+  }
 
 }
