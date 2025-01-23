7c92f2b0193c2facb41df69c6fc2db9a1e76080f
Arthur José Ferro Motter <t_arthur.motter@prefeitura.local>
2025-01-23 17:22:06 -0300

    infinite loop deleted

--- ticket-management-app/client/src/app/app.component.ts
+++ ticket-management-app/client/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { TicketManagerComponent } from './pages/ticket-manager/ticket-manager.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { Subscription } from 'rxjs';
-import { AplicationThemeService } from './services/aplication-theme.service';
 
 @Component({
   selector: 'app-root',
@@ -15,20 +13,4 @@ import { AplicationThemeService } from './services/aplication-theme.service';
 })
 export class AppComponent {
   title = 'ticket-management-app';
-
-  private themeSubscription: Subscription | undefined;
-  private themeService = inject(AplicationThemeService);
-
-  ngOnInit(): void {
-    this.themeSubscription = this.themeService.isDarkMode$.subscribe(isDark => {
-      // This is not ideal but it is safe to set to true initially on every load, since it is already checking localStorage
-      this.themeService.setDarkTheme(isDark)
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.themeSubscription) {
-      this.themeSubscription.unsubscribe();
-    }
-  }
 }
