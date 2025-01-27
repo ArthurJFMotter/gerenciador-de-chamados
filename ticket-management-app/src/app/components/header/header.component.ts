4cce1114aab48a518c71197939cfe27ff70af0ea
Arthur José Ferro Motter <t_arthur.motter@prefeitura.local>
2025-01-27 16:56:14 -0300

    header

--- ticket-management-app/src/app/components/header/header.component.ts
+++ ticket-management-app/src/app/components/header/header.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+//import { AplicationThemeService } from '../../services/aplication-theme.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule, MatDrawer } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatListModule,
+    FormsModule
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  //themeService = inject(AplicationThemeService);
+  isMenuOpen: boolean = false;
+    @ViewChild('drawer') drawer!: MatDrawer;
 
+  ngOnInit(): void {
+  }
+  
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    this.drawer.toggle();
+  }
+
+  toggleTheme(): void {
+    //this.themeService.toggleTheme();
+  }
 }
