8b5709c6facf391bf06dadc7506c2fe42ee8ba28
Arthur José Ferro Motter <t_arthur.motter@prefeitura.local>
2025-01-27 17:01:38 -0300

    footer

--- ticket-management-app/src/app/components/footer/footer.component.ts
+++ ticket-management-app/src/app/components/footer/footer.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule, MatButtonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.css'
 })
-export class FooterComponent {
-
+export class FooterComponent implements OnInit {
+  ngOnInit(): void {
+  }
 }
