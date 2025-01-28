9b17c587c0d0affe072f6bce6b05a53390484b85
ArthurJFMotter <arthur.jf.motter@gmail.com>
2025-01-28 07:41:13 -0300

    wrapper

--- ticket-management-app/src/app/app.component.ts
+++ ticket-management-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PanelComponent } from './pages/panel/panel.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent, PanelComponent],
+  imports: [FooterComponent, HeaderComponent, PanelComponent, WrapperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
