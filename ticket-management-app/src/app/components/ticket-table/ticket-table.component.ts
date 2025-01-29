aa59235bd8c9f7d543f17457a1802e0e1d667c54
Arthur José Ferro Motter <t_arthur.motter@prefeitura.local>
2025-01-29 16:25:19 -0300

    table rollback

--- ticket-management-app/src/app/components/ticket-table/ticket-table.component.ts
+++ ticket-management-app/src/app/components/ticket-table/ticket-table.component.ts
@@ -29,53 +29,30 @@ export class TicketTableComponent implements OnInit, OnChanges {
   @Input() searchTerm: string = '';
 
   displayedTickets = new MatTableDataSource<Ticket>([]);
-  displayedColumns = ['id', 'status', 'requesterName', 'request', 'name', 'locationRegion', 'createdDate', 'lastInteraction', 'selection'];
+  displayedColumns: string[] = ['id', 'status', 'requesterName', 'request', 'locationName', 'locationRegion', 'createdDate', 'lastInteraction', 'responsible', 'selection'];
 
-  columnConfig: { [key: string]: { label: string; name: string } } = {
-    id: { label: 'id', name: '' },
-    status: { label: 'status', name: 'Status' },
-    requesterName: { label: 'requesterName', name: 'Solicitante' },
-    request: { label: 'request', name: 'Solicitação' },
-    name: { label: 'name', name: 'Local' },
-    locationRegion: { label: 'locationRegion', name: 'Região' },
-    createdDate: { label: 'createdDate', name: 'Data' },
-    lastInteraction: { label: 'lastInteraction', name: '' },
-    selection: { label: 'selection', name: '' }
+  columnConfig: { [key: string]: { name: string; icon: string } } = {
+    id: { name: 'ID', icon: 'tag' },
+    status: { name: 'Status', icon: 'flag' },
+    request: { name: 'Solicitação', icon: 'assignment' },
+    requesterName: { name: 'Solicitante', icon: 'account_circle' },
+    locationName: { name: 'Localização', icon: 'place' },
+    locationRegion: { name: 'Região', icon: 'map' },
+    createdDate: { name: 'Data', icon: 'event' },
+    lastInteraction: { name: '', icon: 'update' },
+    responsible: { name: 'Responsável', icon: 'person' },
+    selection: { name: '', icon: 'checklist' },
   };
 
   @ViewChild(MatSort) sort!: MatSort;
 
   ngOnInit(): void {
-    this.updateDisplayedTickets();
-    this.displayedTickets.sort = this.sort;
+    this.displayedTickets.data = this.allTickets;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['allTickets'] || changes['currentPage'] || changes['pageSize'] || changes['searchTerm']) {
-      this.updateDisplayedTickets();
+      this.displayedTickets.data = this.allTickets;
     }
-  }
-
-  updateDisplayedTickets(): void {
-    const filteredTickets = this.filterTickets(this.allTickets, this.searchTerm);
-    const startIndex = (this.currentPage - 1) * this.pageSize;
-    const endIndex = startIndex + this.pageSize;
-    this.displayedTickets.data = filteredTickets.slice(startIndex, endIndex);
-  }
-
-  filterTickets(tickets: Ticket[], term: string): Ticket[] {
-    if (!term) return tickets;
-
-    const lowerCaseTerm = term.toLowerCase();
-    return tickets.filter(ticket =>
-      ticket.id.toString().includes(lowerCaseTerm) ||
-      ticket.status?.toLowerCase().includes(lowerCaseTerm) ||
-      ticket.requester?.name?.toLowerCase().includes(lowerCaseTerm) ||
-      ticket.request?.toLowerCase().includes(lowerCaseTerm) ||
-      ticket.location?.name?.toLowerCase().includes(lowerCaseTerm) ||
-      ticket.location?.region?.toLowerCase().includes(lowerCaseTerm) ||
-      (this.dateService.formatDateToDayMonthYear(ticket.createdDate) || '').toLowerCase().includes(lowerCaseTerm) ||
-      this.dateService.timeSince(this.dateService.formatDateToValidISO(ticket.lastInteraction) || '').toLowerCase().includes(lowerCaseTerm)
-    );
   }
 }
